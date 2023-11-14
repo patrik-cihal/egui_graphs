a721393927788a35b44f2071fe33478666183480
Patrik Cihal <patrik.cihal1234@gmail.com>
2023-11-14 14:32:59 +0100

    Serialize graph container

--- src/graph.rs
+++ src/graph.rs
@@ -9,7 +9,7 @@ use petgraph::{
 use crate::{metadata::Metadata, transform, Edge, Node, SettingsStyle};
 
 /// Graph type compatible with [`super::GraphView`].
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Graph<N: Clone, E: Clone, Ty: EdgeType> {
     pub g: StableGraph<Node<N>, Edge<E>, Ty>,
 }
